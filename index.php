ac55d24b8b5b1bd023d2564d541e72d267f3cb09
Matthew Reidsma <matthew@reidsrow.com>
2012-08-01 20:42:06 -0400

    Added twitter link, simplified footer, added ask lazy web link and started suggest a definition functions.

--- index.php
+++ index.php
@@ -52,6 +52,10 @@ if (isset($_GET['s'])) {
 		
 		<div class="size">
 		
+		<?php
+		if(!isset($_GET['s'])) { // No search. Show the search field.
+		?>
+		
 		<form class="lib-form" method="get" action="">
 		<label for="s">What jargon do you need un-libraried?</label>
 		<input type="text" name="s" <? if(isset($_GET['s'])) { echo 'value="' . $_GET["s"] . '"'; } ?>/>
@@ -59,6 +63,12 @@ if (isset($_GET['s'])) {
 		</form>
 		
 		<?php 
+		
+		} else { // Search has been done, show the "New search" link.
+			
+		echo '<p class="newsearch-link"><a href="/">New Search</a></p>';
+		
+		}
 		
 		if(isset($_GET['s'])) {
 		
@@ -74,6 +84,29 @@ if (isset($_GET['s'])) {
 			echo '<p class="whoops">Wow. You&#8217;ve stumped us.</p>';
 			echo '<p style="margin:1em 0;">We&#8217;ll go ask a librarian and get a definition up soon.</p>';
 			
+			// Give them the option of submitting a definition or asking the "Lazy Web"
+			
+		?>
+		
+			<div class="line">
+				<div class="size1of4 unit">
+					<?php
+					echo '<a href="http://twitter.com/?status=Lazy%20Web!%20How%20would%20you%20define%20' . urlencode($search) . '%20%23unbraryit" class="centered lib-button-small stumped-button">Ask the Lazy Web</a>';
+					?>
+				</div>
+				<div class="size1of4 unit">
+					<?php
+					echo '<a href="/suggest/' . $search . '" class="centered lib-button-small stumped-button">Suggest a definition</a>';
+					?>
+				</div>
+				<div class="size1of2 unit lastUnit">
+				</div>
+			</div>
+		
+		<?php
+			
+			// Record this term so we know someone searched for it
+			
 			$data = array($_GET['s']);
 			if (!$DataFile = fopen("notfound.csv", "a")) {echo "Failure: cannot open file"; die;};
 			if (!fputcsv($DataFile, $data)) {echo "Failure: cannot write to file"; die;};
@@ -85,13 +118,14 @@ if (isset($_GET['s'])) {
 		
 	</div>
 </div>
-	
+		
 	</div>
 	
 	<footer>
+		<p class="twitter"><a href="http://twitter.com/unbraryit">Follow @UnbraryIt on Twitter</a></p>
 		
-		<p class="bylabs">A <a href="http://gvsu.edu/library/labs"><abbr title="Grand Valley State University">GVSU</abbr> Library Labs</a> Joint, inspired by <a href="http://unsuck-it.com">Unsuck It</a>.</p>
-		<p><small>Built by <a href="http://abbybedford.com">Abby Bedford</a>, Ala Alluhaidan, Mary Morgan, Patrick Roth, and <a href="http://matthewreidsma.com">Matthew Reidsma</a> for <a href="http://gvsu.edu/library">Grand Valley State University Libraries</a>.</p>
+		<p class="bylabs">A <a href="http://gvsu.edu/library/labs"><abbr title="Grand Valley State University">GVSU</abbr> Library Labs</a> project, inspired by <a href="http://unsuck-it.com">Unsuck It</a>.</p>
+		<!--p><small>Built by <a href="http://abbybedford.com">Abby Bedford</a>, Ala Alluhaidan, Mary Morgan, Patrick Roth, and <a href="http://matthewreidsma.com">Matthew Reidsma</a> for <a href="http://gvsu.edu/library">Grand Valley State University Libraries</a>.</p-->
 		<p>Licensed under the <a href="http://www.gnu.org/copyleft/gpl.html"><abbr title="GNU General Public License">GPL</abbr></a>. Source code available on <a href="https://github.com/mreidsma/unbraryit">Github</a>.</small></p>
 
 	</footer>
