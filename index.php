f9bd14962fafccb672bf96c0eb0d44b912e475d5
Matthew Reidsma <matthew@reidsrow.com>
2012-08-03 11:33:00 -0400

    Added definition suggest feature, removed GVSU branding, added ask lazy web feature, started share this definition feature

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_GET['s'])) {
+if(isset($_GET['s'])) {
 	
 	include 'url.php';
 	
@@ -25,6 +25,68 @@ if (isset($_GET['s'])) {
   	}
 }
 
+if(isset($_POST['addnew'])) {
+	
+	$m = NULL;
+	
+	// Send an email to the admin account
+	
+	include 'url.php';
+	
+	$name = stripslashes($_POST['name']);
+	$email = stripslashes($_POST['email']);
+	$term = stripslashes($_POST['term']);
+	$definition = stripslashes($_POST['definition']);
+	$agree = $_POST['agree'];
+	
+	if($agree != 1) {
+		
+		$m = '<p class="lib-error">You must agree to the terms.</p>';
+		
+	} else {
+		
+		if($name == NULL) {
+			
+			$m = '<p class="lib-error">You need to tell us your name.</p>';
+			
+		} else {
+			
+			if($email == NULL) {
+
+				$m = '<p class="lib-error">You need to include your email address.</p>';
+
+			} else {
+				
+				if($term == NULL) {
+
+					$m = '<p class="lib-error">How do I know what term you want to define? You forgot to tell me.</p>';
+
+				} else {
+					
+					if($definition == NULL) {
+
+						$m = '<p class="lib-error">That&#8217;s hardly a definition.</p>';
+
+					}
+				}}}}
+				
+	if($m == NULL) { // No errors, send the email
+		
+		$message = $term . ': ' . $definition . '\n\n';
+		$message .= 'Submitted by: ' . $name . ' ' . $email;
+		$subject = "New Definition";
+		
+		// compose headers
+		$headers = "From: hello@unbraryit.com\r\n";
+		$headers .= "Reply-To: hello@unbraryit.com\r\n";
+		$headers .= "X-Mailer: PHP/".phpversion();
+		
+		mail($to, $subject, $message, $headers);
+		
+		$m = '<p class="lib-success">Your suggestion has been sent. Thanks!</p>';
+		 
+	}
+}
 ?>
 
 <html lang="en">
@@ -53,10 +115,10 @@ if (isset($_GET['s'])) {
 		<div class="size">
 		
 		<?php
-		if(!isset($_GET['s'])) { // No search. Show the search field.
+		if((!isset($_GET['s'])) && (!isset($_GET['a']))) { // No search and no add. Show the search field.
 		?>
 		
-		<form class="lib-form" method="get" action="">
+		<form class="lib-form jargon" method="get" action="">
 		<label for="s">What jargon do you need un-libraried?</label>
 		<input type="text" name="s" <? if(isset($_GET['s'])) { echo 'value="' . $_GET["s"] . '"'; } ?>/>
 		<input type="submit" value="Unbrary It" class="lib-button-small" />
@@ -65,9 +127,9 @@ if (isset($_GET['s'])) {
 		<?php 
 		
 		} else { // Search has been done, show the "New search" link.
-			
-		echo '<p class="newsearch-link"><a href="/">New Search</a></p>';
 		
+			echo '<p class="newsearch-link lib-button-small-grey"><a href="/">New Search</a></p>';
+	
 		}
 		
 		if(isset($_GET['s'])) {
@@ -77,8 +139,9 @@ if (isset($_GET['s'])) {
 		echo '<dl>
 			<dt>' . $term_match . '</dt>
 			<dd>
-				<p>' . $definition . '</p>
-				<p class="twitter"><a href="http://twitter.com/?status=' . urlencode($term) . '%3A%20http%3A%2F%2Funbraryit.com%2F%3Fs%3D' . $search . '%20%40unbraryit">Share this on Twitter</a></p>
+				<p>' . $definition . '</p>';
+				// Twitter strips + and encoded forms from URLs - not sure how to pass them to Twitter
+				// <p class="twitter"><a href="http://twitter.com/?status=' . urlencode($term_match) . '%3A%20http%3A%2F%2Funbraryit.com%2F%3Fs%3D' . urlencode($search) . '%20%40unbraryit">Share this on Twitter</a></p>
 			</dd>
 		</dl>';
 		
@@ -94,12 +157,12 @@ if (isset($_GET['s'])) {
 			<div class="line">
 				<div class="size1of4 unit">
 					<?php
-					echo '<a href="http://twitter.com/?status=Lazy%20Web!%20How%20would%20you%20define%20' . urlencode($search) . '%20%23unbraryit" class="centered lib-button-small stumped-button">Ask the Lazy Web</a>';
+					echo '<a href="http://twitter.com/?status=Lazy%20Web!%20How%20would%20you%20define%20' . str_replace(" ", "%2B", $search) . '%3F%20%23unbraryit" class="centered lib-button-small stumped-button">Ask the Lazy Web</a>';
 					?>
 				</div>
 				<div class="size1of4 unit">
 					<?php
-					echo '<a href="/suggest/' . $search . '" class="centered lib-button-small stumped-button">Suggest a definition</a>';
+					echo '<a href="?a=' . $search . '" class="centered lib-button-small stumped-button">Suggest a definition</a>';
 					?>
 				</div>
 				<div class="size1of2 unit lastUnit">
@@ -116,6 +179,31 @@ if (isset($_GET['s'])) {
 			fclose($DataFile);
 			
 		}	
+		
+		}
+		
+		if(isset($_GET['a'])) { // User is suggesting a new definition
+			
+			echo $m; // Show any errors
+			
+		?>
+		
+			<form class="lib-form" name="add-definition" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+				<label for="term">Your Jargon:</label>
+				<input type="text" name="term" required="required" value="<?php echo $_GET['a']; ?>" />
+				<label for="definition">Unbrary it for us:</label>
+				<textarea name="definition" required="required"><?php if(isset($definition)) { echo $definition; } ?></textarea>
+				<label for="name">Your Name:</label>
+				<input type="text" name="name" required="required" <?php if(isset($name)) { echo 'value="' . $name . '"'; } ?> />
+				<label for="email">Your email:</label>
+				<input type="email" name="email" required="required" <?php if(isset($email)) { echo 'value="' . $email . '"'; } ?> />
+				<p><small><strong>Terms:</strong> By submitting this, you agree that you have permission to post it and that UnbraryIt might use your definition as-is or edited without crediting you. Because really, you do not want your name associated with this. Trust us.</small></p>
+				<input type="checkbox" name="agree" required="required" value="1" /> <label class="lib-inline" for="agree">I agree to the terms</label>
+				<input class="lib-button-small" type="submit" name="addnew" value="Suggest" />
+			</form>
+		
+		<?php
+			
 		}
 		?>
 		
@@ -127,8 +215,6 @@ if (isset($_GET['s'])) {
 	<footer>
 		<p class="twitter"><a href="http://twitter.com/unbraryit">Follow @UnbraryIt on Twitter</a></p>
 		
-		<p class="bylabs">A <a href="http://gvsu.edu/library/labs"><abbr title="Grand Valley State University">GVSU</abbr> Library Labs</a> project, inspired by <a href="http://unsuck-it.com">Unsuck It</a>.</p>
-		<!--p><small>Built by <a href="http://abbybedford.com">Abby Bedford</a>, Ala Alluhaidan, Mary Morgan, Patrick Roth, and <a href="http://matthewreidsma.com">Matthew Reidsma</a> for <a href="http://gvsu.edu/library">Grand Valley State University Libraries</a>.</p-->
 		<p>Licensed under the <a href="http://www.gnu.org/copyleft/gpl.html"><abbr title="GNU General Public License">GPL</abbr></a>. Source code available on <a href="https://github.com/mreidsma/unbraryit">Github</a>.</small></p>
 
 	</footer>
